6dcc45a09c081f9932d1f5d3643cb6964e029ff2
Shawn Kang <skang123@uw.edu>
2017-10-14 01:04:17 -0700

    Add implementation for synchronous down counter

---
+++ Project1/syncDownCounter.v
@@ -0,0 +1,23 @@
+`include "dFlipFlop.v"
+
+module SynchronousDownCounter(out[3:0], clock, reset);
+	input clock, reset;
+	output [3:0] out;
+
+	wire dOne, qOne, qBarOne;
+	wire dTwo, qTwo, qBarTwo;
+	wire dThree, qThree, qBarThree;
+	wire dFour, qFour, qBarFour;
+
+	assign out[3:0] = {qFour, qThree, qTwo, qOne};
+
+	assign dOne = qBarOne;
+	assign dTwo = qBarOne ^ qTwo;
+	assign dThree = (qBarTwo & qBarOne) ^ qThree;
+	assign dFour = (qBarThree & qBarTwo & qBarOne) ^ qFour;
+
+	DFlipFlop one(qOne, qBarOne, dOne, clock, reset);
+	DFlipFlop two(qTwo, qBarTwo, dTwo, clock, reset);
+	DFlipFlop three(qThree, qBarThree, dThree, clock, reset);
+	DFlipFlop four(qFour, qBarFour, dFour, clock, reset);
+endmodule
---
+++ Project1/syncDownCounterTestbench.v
@@ -0,0 +1,71 @@
+`include "syncDownCounter.v"
+
+module testbench();
+	wire clock, reset;
+	wire[3:0] out;
+
+	SynchronousDownCounter counter(out[3:0], clock, reset);
+
+	Tester test(out[3:0], clock, reset);
+
+	// file for gtkwave
+  	initial 
+  	begin
+  		$dumpfile("syncDownCounter.vcd");
+  		$dumpvars(1, counter);
+	end 
+endmodule
+
+module Tester(out[3:0], clock, reset);
+	input [3:0] out;
+	output clock, reset;
+	reg clock, reset;
+
+	initial  // Response
+  	begin
+	$display("\t\t clock  reset \t output \t    Time ");
+    	$monitor("\t\t %b\t %b  \t %b", clock, reset, out, 
+								  $time );
+  	end
+
+	parameter stimDelay = 20;
+
+	initial  // Stimulus
+	  begin
+	               reset = 'b0; clock = 'b0;
+	    #stimDelay reset = 'b1;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+	    #stimDelay clock = 'b1;
+	    #stimDelay clock = 'b0;
+		
+	    #(2*stimDelay);            	// needed to see END of simulation
+	    $finish;                   		// finish simulation
+	  end
+endmodule
