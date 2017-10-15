c344ff5148eada44549016f66795a3ff9b92179f
Shawn Kang <skang123@uw.edu>
2017-10-15 15:49:43 -0700

    Change first counter from up to down

--- Project1/rippleUpCounter.v
+++ Project1/rippleUpCounter.v
@@ -1,16 +1,16 @@
 `include "dFlipFlop.v"
 
-module RippleUpCounter(out[0:3], clock, reset);
+module RippleUpCounter(out[3:0], clock, reset);
 	input clock, reset;
-	output [0:3] out;
+	output [3:0] out;
 
 	wire qBarOne;
 	wire qBarTwo;
 	wire qBarThree;
 	wire qBarFour;
 
-	DFlipFlop one(out[3], qBarOne, qBarOne, clock, reset);
-	DFlipFlop two(out[2], qBarTwo, qBarTwo, out[3], reset);
-	DFlipFlop three(out[1], qBarThree, qBarThree, out[2], reset);
-	DFlipFlop four(out[0], qBarFour, qBarFour, out[1], reset);
+	DFlipFlop one(out[0], qBarOne, qBarOne, clock, reset);
+	DFlipFlop two(out[1], qBarTwo, qBarTwo, qBarOne, reset);
+	DFlipFlop three(out[2], qBarThree, qBarThree, qBarTwo, reset);
+	DFlipFlop four(out[3], qBarFour, qBarFour, qBarThree, reset);
 endmodule
