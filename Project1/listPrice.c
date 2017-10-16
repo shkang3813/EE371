8b3dced9c60f1023a824985cd6d6f75c5c4db378
Shawn Kang <skang123@uw.edu>
2017-10-16 15:34:07 -0700

    Add integer checking to listPrice

--- Project1/listPrice.c
+++ Project1/listPrice.c
@@ -1,28 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main() {
-	int manufacturerCost;
-	int markupCost;
-	double salesTax;
-	double discount;
+static bool isInteger(char *input);
 
-	double finalPrice;
+int main(int argc, char **argv) {
+	// Initially declare all inputs as strings
+	char *manufacturerCostString;
+	char *markupCostString;
+	char *salesTaxString;
+	char *discountString;
 
 	printf("Enter Manufacturer's Cost: \n");
-	scanf("%d", &manufacturerCost);
+	scanf("%s", &manufacturerCostString);
 	printf("Enter Dealer's Markup Cost: \n");
-	scanf("%d", &markupCost);
+	scanf("%s", &markupCostString);
 	printf("Enter Local Sales Tax Percentage (0.0 - 100.0): \n");
-	scanf("%lf", &salesTax);
+	scanf("%s", &salesTaxString);
 	printf("Enter Pre Tax Discount Percentage (0.0 - 100.0): \n");
-	scanf("%lf", &discount);
-
+	scanf("%s", &discountString);
 	printf("\n");
+
+	// Check if the inputs are valid
+	if (!isInteger(manufacturerCostString)) {
+		return EXIT_FAILURE;
+	}
+	if (!isInteger(markupCostString)) {
+		return EXIT_FAILURE;
+	}
+
+	int manufacturerCost = atoi(manufacturerCostString);
+	int markupCost = atoi(markupCostString);
+	double salesTax = atof(salesTaxString);
+	double discount = atof(discountString);
+
+	double finalPrice;
 
 	finalPrice = (manufacturerCost + markupCost) * (1.0 - (discount / 100.0)) * (1.0 + (salesTax / 100.0)) ;
 	printf("Final Estimated Price: %f \n",finalPrice);
 
-	return 0;
-
+	return EXIT_SUCCESS;
 }
 
+// Checks whether the given string represents an integer
+// Returns true if the given string represents an integer
+static bool isInteger(char *input) {
+        int length = strlen(input);
+        for (int index = 0; index < length; index++) {
+                char current = input[index];
+                if (!isdigit(current)) {
+                        return 0;
+                }
+        }
+        return 1;
+}
