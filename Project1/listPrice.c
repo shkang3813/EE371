03b5b001489ca323ec00ca426e59423d7b045768
Evan Zien <ezien@uw.edu>
2017-10-19 13:35:11 -0700

    design fix

--- Project1/listPrice.c
+++ Project1/listPrice.c
@@ -1,59 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-static bool isInteger(char *input);
-
-int main(int argc, char **argv) {
-	// Initially declare all inputs as strings
-	char *manufacturerCostString;
-	char *markupCostString;
-	char *salesTaxString;
-	char *discountString;
-
-	printf("Enter Manufacturer's Cost: \n");
-	scanf("%s", manufacturerCostString);
-	printf("Enter Dealer's Markup Cost: \n");
-	scanf("%s", markupCostString);
-	printf("Enter Local Sales Tax Percentage (0.0 - 100.0): \n");
-	scanf("%s", salesTaxString);
-	printf("Enter Pre Tax Discount Percentage (0.0 - 100.0): \n");
-	scanf("%s", discountString);
-	printf("\n");
-
-	// Check if the inputs are valid
-	if (!isInteger(manufacturerCostString)) {
-		printf("Manufacturer cost must be an integer!\n")
-		return EXIT_FAILURE;
-	}
-	if (!isInteger(markupCostString)) {
-		printf("Markup cost must be an integer!\n")
-		return EXIT_FAILURE;
-	}
-
-	int manufacturerCost = atoi(manufacturerCostString);
-	int markupCost = atoi(markupCostString);
-	double salesTax = atof(salesTaxString);
-	double discount = atof(discountString);
+int main() {
+	int manufacturerCost;
+	int markupCost;
+	double salesTax;
+	double discount;
 
 	double finalPrice;
+
+	printf("Enter Manufacturer's Cost: \n");
+	scanf("%d", &manufacturerCost);
+	printf("Enter Dealer's Markup Cost: \n");
+	scanf("%d", &markupCost);
+	printf("Enter Local Sales Tax Percentage (0.0 - 100.0): \n");
+	scanf("%lf", &salesTax);
+	printf("Enter Pre Tax Discount Percentage (0.0 - 100.0): \n");
+	scanf("%lf", &discount);
+
+	printf("\n");
 
 	finalPrice = (manufacturerCost + markupCost) * (1.0 - (discount / 100.0)) * (1.0 + (salesTax / 100.0)) ;
 	printf("Final Estimated Price: %f \n",finalPrice);
 
-	return EXIT_SUCCESS;
-}
+	return 0;
 
-// Checks whether the given string represents an integer
-// Returns true if the given string represents an integer
-static int isInteger(char *input) {
-    int length = strlen(input);
-    for (int index = 0; index < length; index++) {
-        char current = input[index];
-        if (!isdigit(current)) {
-            return 0;
-        }
-    }
-    return 1;
 }
