750dda67fdf59cb69ffc84660393174d35dc4690
Tyler S. Horiuchi <tylerh96@uw.edu>
2017-11-26 21:19:48 -0800

    nios stuff

---
+++ lab4/niosII_hw_dev_tutorial/DE1_SoC.sv
@@ -0,0 +1,15 @@
+module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
+	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	output logic [9:0] LEDR;
+	input logic [3:0] KEY;
+	input logic [9:0] SW;
+	input logic CLOCK_50;
+	
+	first_nios2_system sys(
+		.clk_clk(CLOCK_50),                          //                       clk.clk
+		.pio_0_external_connection_export(LEDR[7:0]), // pio_0_external_connection.export
+		.reset_reset_n(KEY[0])                  //                     reset.reset_n
+	);
+	
+	
+endmodule
