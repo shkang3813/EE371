02f17d9a100e5e1955cdef5ed7c44c4a82b76940
Shawn Kang <skang123@uw.edu>
2017-12-07 03:31:39 -0800

    Self code

---
+++ lab4/CharLoop/software/full_camera/obj/default/hello_world_small.d
@@ -0,0 +1,17 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../full_camera_bsp//HAL/inc/sys/alt_stdio.h \
+ ../full_camera_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../full_camera_bsp//HAL/inc/io.h ../full_camera_bsp//HAL/inc/alt_types.h \
+ ../full_camera_bsp/system.h ../full_camera_bsp/linker.h
+
+../full_camera_bsp//HAL/inc/sys/alt_stdio.h:
+
+../full_camera_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../full_camera_bsp//HAL/inc/io.h:
+
+../full_camera_bsp//HAL/inc/alt_types.h:
+
+../full_camera_bsp/system.h:
+
+../full_camera_bsp/linker.h:
